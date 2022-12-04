c4b2f5004fe00f5ec6c010fa436588f555822bd2
Beazlez <fifthbeasley@gmail.com>
2022-12-04 06:14:11 -0800

    Changes to Password Generator and Testing

---
+++ Password Generator.py
@@ -0,0 +1,6 @@
+import random
+
+Roll = 1, 2, 3, 4, 5, 6, 
+
+
+
---
+++ Testing.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import random
+
+
+window = Tk()
+window.geometry("900x600")
+window.config(bg="#2B56A1")
+window.resizable(0 ,0)
+window.title("Random Number Generator")
