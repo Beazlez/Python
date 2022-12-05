9576e4529adabc0708b04bdbe11eab5f31e63eff
Beazlez <fifthbeasley@gmail.com>
2022-12-05 06:24:01 -0800

    Messing with Numbers; Changes to Order Bot; Notes

--- Testing.py
+++ Testing.py
@@ -1,17 +1,27 @@
 #print("""This is a test
  #   I am Testing
   #  Wow, a lot of Testing""")
-#print("so now if I type \n testing")
-#print("This is a test" + " \nI am adding these together" + " \nAdding one more")
 
-#I am messing with the print Function
+# Using Triple Quotes will Allow me to have the Print Function on Multiple Lines (See in Action on Code lines 1-3)
+#If I type in \n that will put text on a new line (See in Action on Code lines 12&17)
 
-print("Hello, Welcome to my Store!")
+#Anything Between Quotes is a String
 
-name = input("What is your name?\n")
+#This is a little Food Server Bot Thing I Made // I was messing around with Inputs and Variables
+  #print("Hello, Welcome to my Store!")
 
-print("Hello " + name + ", Thank you for coming into my Store! Here is a Menu, What Would You like to Order?")
-Menu = ("Pizza, Waffles, Taco Salad")
-print(Menu)
-Menu = input()
-print("Thank you for Odering " + Menu + " It will be Ready Shortly!")
+  #name = input("What is your name?\n")
+
+  #print("Hello " + name + ", Thank you for coming into my Store! Here is a Menu")
+  #Menu = ("Pizza , Waffles , Taco Salad ")
+  #print(Menu)
+  #Order = input("What would You Like to Order?\n")
+  #print("Thank you for Ordering " + Order, name + ". It will be Ready Shortly!")
+
+#Following Along with NetChuck's Ep 3 of Python // Messing around with Numbers Now
+#The Number Alone is an Integer // If I put Quotes around it, the number becomes a String
+#Name Variable = String // Age Variable = Integer // Acutal_Age Variable = Floating Point Number AKA Float
+name = ("Beaz")
+age = 23
+actual_age = 23.56
+print(type(actual_age))
